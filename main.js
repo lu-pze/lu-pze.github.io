fab473459e800a8ee70950ed8ebb766572a4c145
Pex Tufvesson <pex@livet.se>
2024-03-12 16:15:47 +0100

    Reducing number of external fonts to minimize Google information leakage.

--- main.js
+++ main.js
@@ -1579,6 +1579,7 @@ function next_quiz (){
   redraw(); // Needed to get the title of the Dirac Impulse response correct
 }
 
+let cap_grade=0;
 
 function update_quiz(){
   if (current_quiz == "none") return;
@@ -1594,18 +1595,21 @@ function update_quiz(){
   }
   quiz_difficulty = difficulty_mean/quiz_questions.length;
 
-  let cap_grade=0;
+  let new_cap_grade=0;
   if (quiz_difficulty<70){
-    cap_grade=0; // Black
+    new_cap_grade=0; // Black
   } else if (quiz_difficulty<80){
-    cap_grade=1; // Bronze
+    new_cap_grade=1; // Bronze
   } else if (quiz_difficulty<98){
-    cap_grade=2; // Silver
+    new_cap_grade=2; // Silver
   } else {
-    cap_grade=3; // Gold
+    new_cap_grade=3; // Gold
   }
-  let cap_0 = document.getElementById("cap_0");
-  cap_0.src = "images/cap_"+cap_grade+".png";
+  if (new_cap_grade != cap_grade){
+    let cap_0 = document.getElementById("cap_0");
+    cap_0.src = "images/cap_"+cap_grade+".png";
+    cap_grade=new_cap_grade;
+  }
   let cap_0_text = document.getElementById("cap_0_text");
   cap_0_text.innerHTML = quiz_difficulty.toFixed(0) + "/100&nbsp";
 
@@ -2251,6 +2255,7 @@ function select_assignment(event){
   }
 }
 
+let assignment_grade=0;
 function update_tasks (){
   let task_div=document.getElementById("task_list");
   if (current_assignment=="none"){
@@ -2259,18 +2264,21 @@ function update_tasks (){
   }
 
   let assignment_score = 100.0 * done_tasks.length / Object.keys(all_tasks).length;
-  let assignment_grade=0;
+  let new_assignment_grade=0;
   if (assignment_score<15){
-    assignment_grade=0; // Black
+    new_assignment_grade=0; // Black
   } else if (assignment_score<50){
-    assignment_grade=1; // Bronze
+    new_assignment_grade=1; // Bronze
   } else if (assignment_score<98){
-    assignment_grade=2; // Silver
+    new_assignment_grade=2; // Silver
   } else {
-    assignment_grade=3; // Gold
+    new_assignment_grade=3; // Gold
   }
-  let assignment_0 = document.getElementById("assignment_0");
-  assignment_0.src = "images/assignment_"+assignment_grade+".png";
+  if (new_assignment_grade != assignment_grade){
+    let assignment_0 = document.getElementById("assignment_0");
+    assignment_0.src = "images/assignment_"+assignment_grade+".png";
+    assignment_grade = new_assignment_grade;
+  }
   let assignment_0_text = document.getElementById("assignment_0_text");
   assignment_0_text.innerHTML = done_tasks.length + "/" + Object.keys(all_tasks).length + "&nbsp";
 
@@ -2435,22 +2443,26 @@ const achievement_ranks={
   0:"Automatic Control Newbie"
 }
 
+let star_grade=0;
 function update_achievements (){
   achievement_score = 100.0 * done_achievements.length / Object.keys(all_achievements).length;
   achievement_rank="";
   achievement_score_to_next_rank=-1;
-  let star_grade=0;
+  let new_star_grade=0;
   if (achievement_score<15){
-    star_grade=0; // Black
+    new_star_grade=0; // Black
   } else if (achievement_score<50){
-    star_grade=1; // Bronze
+    new_star_grade=1; // Bronze
   } else if (achievement_score<98){
-    star_grade=2; // Silver
+    new_star_grade=2; // Silver
   } else {
-    star_grade=3; // Gold
+    new_star_grade=3; // Gold
   }
-  let star_0 = document.getElementById("star_0");
-  star_0.src = "images/star_"+star_grade+".png";
+  if (new_star_grade != star_grade){
+    let star_0 = document.getElementById("star_0");
+    star_0.src = "images/star_"+star_grade+".png";
+    star_grade=new_star_grade;
+  }
   let star_0_text = document.getElementById("star_0_text");
   star_0_text.innerHTML = done_achievements.length + "/" + Object.keys(all_achievements).length + "&nbsp";
 
